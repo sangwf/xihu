d0fe30fb6aec103eb0fd82d183af99522f3d8225
Wenfeng Sang <sangwf@gmail.com>
2016-02-25 10:30:37 +0800

    Write hello world on Mountain Gu.

---
+++ helloxihu.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  helloxihu.c
+ *
+ *    Description:  Write hello world on Mountain Gu.
+ *
+ *        Version:  1.0
+ *        Created:  2016/02/24 12时07分49秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Sang Wenfeng, sangwenfeng@sensorsdata.cn
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+    printf("hello, world\n");
+
+    return 0;
+}
+
